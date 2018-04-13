41b00af0430fcd0ffbf3387394e7bb6af5980b36
Rolando Cruz <rolando.cruz21@gmail.com>
2018-04-13 12:13:40 +0800

    [me2] Added main

--- me2/cruz_rolando.hs
+++ me2/cruz_rolando.hs
@@ -11,7 +11,15 @@ term :: (Floating a, Integral b) => a -> b -> a
 term x y = (x ^^ y) / fromIntegral(factorial y)
 
 series :: (Floating a) => a -> [a]
-series x = [sign * (term x exp) | (exp, sign) <- zip [1,3..] (cycle [1, -1])]
+series x = zipWith (*) ([term x exp | exp <- [1,3..]]) (cycle [1, -1])
 
 sin' :: (Floating a) => a -> a
 sin' x = foldl (+) 0 (take 30 (series x))
+
+test :: (Num a) => a -> a
+test x = (2 * x ^ 2) + (5 * x) + 12
+
+main = do
+  print(simpsons test 1 3)
+  print(trapezoidal test 1 3)
+  print(sin' (3 * pi / 2))
