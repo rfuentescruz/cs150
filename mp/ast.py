d667599d7c6a54fdc796ea3c4232d0a6e0316aff
Rolando Cruz <rolando.cruz21@gmail.com>
2018-04-19 01:15:44 +0800

    [mp] Initial version with data type support

---
+++ mp/ast.py
@@ -0,0 +1,132 @@
+class LexicalError(Exception):
+    def __init__(self, node):
+        self.node = node
+
+
+class LookupError(LexicalError):
+    def __init__(self, node, name):
+        self.node = node
+        self.name = name
+
+    def __str__(self):
+        return 'Undefined name: %s' % self.name
+
+class Scope(object):
+    def __init__(self, parent=None):
+        self.names = {}
+        self.parent = parent
+
+    def __getitem__(self, name):
+        try:
+            return self.names[name]
+        except KeyError:
+            if self.parent is not None:
+                return self.parent[name]
+            else:
+                raise LookupError(node=None, name=name)
+
+    def __setitem__(self, name, value):
+        self.names[name] = value
+
+
+root_scope = Scope()
+
+
+class Node(object):
+    def __init__(
+        self,
+        line=None, children=None, parent=None, scope=None
+    ):
+        self.line = line
+        self.children = children
+        self.parent = parent
+        self.scope = scope if scope else root_scope
+
+
+class Statement(Node):
+    def execute(self):
+        pass
+
+
+class Expression(Node):
+    def evaluate(self):
+        return None
+
+
+class StatementList(Node):
+    def execute(self):
+        for stmt in self.children:
+            if not isinstance(stmt, Statement):
+                raise LexicalError(node=self)
+            stmt.execute()
+
+
+class Assign(Statement):
+    def __init__(self, name, expr, *args, **kwargs):
+        super(Assign, self).__init__(*args, **kwargs)
+        self.name = name
+        if not isinstance(expr, Expression):
+            raise LexicalError(node=self)
+
+        self.expr = expr
+
+    def execute(self):
+        self.scope[self.name] = self.expr.evaluate()
+
+
+class Print(Statement):
+    def __init__(self, expr, *args, **kwargs):
+        super(Print, self).__init__(*args, **kwargs)
+        self.expr = expr
+
+    def execute(self):
+        print self.expr.evaluate()
+
+
+class Lookup(Expression):
+    def __init__(self, name, *args, **kwargs):
+        super(Lookup, self).__init__(*args, **kwargs)
+        self.name = name
+
+    def evaluate(self):
+        return self.scope[self.name]
+
+
+class Literal(Expression):
+    def __init__(self, value, *args, **kwargs):
+        super(Literal, self).__init__(*args, **kwargs)
+        self.value = value
+
+    def evaluate(self):
+        return self.value
+
+
+class List(Expression):
+    def __init__(self, items, *args, **kwargs):
+        super(List, self).__init__(*args, **kwargs)
+        self.items = items if items else []
+
+    def evaluate(self):
+        return [i.evaluate() for i in self.items]
+
+
+class Index(Expression):
+    def __init__(self, target, index, *args, **kwargs):
+        super(Index, self).__init__(*args, **kwargs)
+        if not isinstance(index, Expression):
+            raise LexicalError(node=self)
+
+        self.target = target
+        self.index = index
+
+    def evaluate(self):
+        target = self.target.evaluate()
+        if not isinstance(target, (str, list)):
+            raise LexicalError(node=self)
+
+        index = self.index.evaluate()
+
+        if not isinstance(index, int):
+            raise LexicalError(node=self)
+
+        return target[index]
---
+++ mp/lang.py
@@ -0,0 +1,129 @@
+import sys
+
+from ply import lex, yacc
+
+from ast import *
+
+RESERVED = {
+    'while': 'WHILE'
+}
+
+tokens = [
+    'FLOAT', 'INTEGER', 'NAME', 'NEWLINE', 'PRINT', 'STRING'
+] + list(RESERVED.values())
+
+literals = ['=', '[', ']', ',']
+
+
+def t_FLOAT(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+def t_STRING(t):
+    r'(?x)(?<!\\)".*?(?<!\\)"'
+    t.value = t.value[1:-1].decode("string-escape")
+    return t
+
+
+def t_PRINT(t):
+    r'print '
+    return t
+
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    return t
+
+t_ignore = ' \t'
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+
+
+lex.lex()
+
+def p_main(p):
+    """main : statement_list
+            | statement_list NEWLINE"""
+    p[1].execute()
+
+
+def p_statement_list(p):
+    """statement_list : statement
+                      | statement_list NEWLINE statement"""
+    if len(p) > 2:
+        p[1].children.append(p[3])
+        p[0] = p[1]
+    else:
+        p[0] = StatementList(line=p.lineno, children=[p[1]])
+
+
+def p_statement_print(p):
+    'statement : PRINT expression'
+    p[0] = Print(expr=p[2])
+
+
+def p_statement_assign(p):
+    'statement : NAME "=" expression'
+    p[0] = Assign(name=p[1], expr=p[3], line=p.lineno)
+
+
+def p_expression_index(p):
+    'expression : expression "[" expression "]"'
+    p[0] = Index(target=p[1], index=p[3], line=p.lineno)
+
+
+def p_expression_atom(p):
+    '''expression : atom
+                  | "[" atom_list "]"'''
+    if len(p) > 2:
+        p[0] = p[2]
+    else:
+        p[0] = p[1]
+
+
+def p_atom_list(p):
+    '''atom_list : atom
+                 | atom_list ',' atom'''
+    if len(p) > 2:
+        p[1].items.append(p[3])
+        p[0] = p[1]
+    else:
+        p[0] = List(items=[p[1]], line=p.lineno)
+
+
+def p_atom_name(p):
+    'atom : NAME'
+    p[0] = Lookup(name=p[1])
+
+
+def p_atom_number(p):
+    '''atom : FLOAT
+            | INTEGER
+            | STRING'''
+    p[0] = Literal(value=p[1], line=p.lineno)
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
+
+yacc.yacc()
+yacc.parse('''a = 1
+b = 1.0
+c = [1, 2, 3, "abc", b]
+print [1,2,3,4][1]
+''')
